22a1a80b1181190cb31f405a1b1a11fd782f5f59
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-20 00:18:33 -0400

    tailwind added to home page

--- port-folio/app/page.tsx
+++ port-folio/app/page.tsx
@@ -1,15 +1,43 @@
 import { getProjects } from "@/sanity/sanity-utils"
-import { Project } from "@/types/Project"
+import Image from 'next/image'
 
 export default async function Home() {
 
   const projects = await getProjects()
 
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div
+      className="max-w-5xl mx-auto py-20">
+
+      <h1 className="text-5xl font-extrabold">Hello, I&apos;m <span className="bg-gradient-to-r from-stone-500 via-blue-400 to-orange-500 bg-clip-text text-transparent">Asad</span>!</h1>
+
+      <p className="mt-3 text-xl text-gray-600">Check out my projects</p>
+
+      <h2 className="mt-24 font-bold text-gray-700  text-3xl">
+
+      </h2>
+
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+        {projects.map((project) => (
+          <div key={project._id}
+            className="border border-gray-500 rounded-lg "
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={250}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+
+            <div className="bg-gradient-to-r from-stone-500 via-blue-400 to-orange-500 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
