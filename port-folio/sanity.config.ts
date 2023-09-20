4f5ea2e79677d1fe6f30c7d8f46792cc777cda12
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-19 23:08:41 -0400

    created document type for project

--- port-folio/sanity.config.ts
+++ port-folio/sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import project from "./sanity/schemas/project-schema";
 
 const config = defineConfig({
     projectId: 'bkrqoana',
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: 'My Portfolio',
     apiVersion: "2023-09-19",
     basePath: "/admin",
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: { types: [project] }
 })
 
 
