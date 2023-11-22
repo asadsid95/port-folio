651e98e2343afeeaabbb357f6ea5e5ca0a739769
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-11-21 22:21:14 -0500

    styling: responsive for projects list section ; #9

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -1,96 +1,110 @@
-import { getProject, getProjects } from "@/sanity/sanity-utils"
-import { PortableText } from '@portabletext/react'
-import Image from "next/image"
+import { getProject, getProjects } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
-    params: { project: string }
-}
+  params: { project: string };
+};
 
 export default async function Project({ params }: Props) {
+  const slug = params.project;
 
-    const slug = params.project
+  const project = await getProject(slug);
 
-    const project = await getProject(slug)
+  return (
+    <div className="max-w-3xl mx-auto py-5 text-gray-400 h-[50vh] text-center ">
+      <header className=" cursor-pointer hover:underline bg-black py-10">
+        <a
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="  text-5xl drop-shadow-sm font-extrabold">
+            {project.name}
+          </h1>
+        </a>
+      </header>
 
-    return (
-        <div className="max-w-3xl mx-auto py-5 text-gray-400 h-[50vh] text-center ">
-            <header className=" cursor-pointer hover:underline bg-black py-10">
-                <a href={project.url} title='View Project' target='_blank' rel='noopener noreferrer'>
-                    <h1 className="  text-5xl drop-shadow-sm font-extrabold">
-                        {project.name}
-                    </h1>
-                </a>
+      {project.content && (
+        <div className="text-xl  text-gray-400 mt-5 font-bold">
+          {/* <p className="">Project Description:<br /></p> */}
+          <div>
+            <PortableText value={project.content} />
+          </div>
+        </div>
+      )}
 
-            </header>
+      {project.listOfTechnologies && (
+        <div className="text-lg  text-gray-400 my-5">
+          <p className="underline font-bold">
+            Technologies:
+            <br />
+          </p>
+          <div>
+            <PortableText value={project.listOfTechnologies} />
+          </div>
+        </div>
+      )}
 
-            {project.content && (
-
-                <div className="text-xl  text-gray-400 mt-5 font-bold">
-                    {/* <p className="">Project Description:<br /></p> */}
-                    <div >
-
-                        <PortableText value={project.content} />
-                    </div>
-                </div>
-
-            )}
-
-            {project.listOfTechnologies && (
-
-                <div className="text-lg  text-gray-400 my-5">
-                    <p className="underline font-bold">Technologies:<br /></p>
-                    <div>
-
-                        <PortableText value={project.listOfTechnologies} />
-                    </div>
-                </div>
-
-            )}
-
-            {project.role && project && project.responsibilities && (
-
-                <div className="flex justify-center gap-5">
-                    <div className="flex flex-col">
-                        <p className="underline font-bold">Role:<br /></p>
-                        <div className="text-base">
-
-                            <p>
-                                {project.role}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col">
-
-                        <p className="underline font-bold">Responsibilities:<br /></p>
-                        <div className="text-base over">
-                            {/* <ul>
+      {project.role && project && project.responsibilities && (
+        <div className="flex justify-center gap-5">
+          <div className="flex flex-col">
+            <p className="underline font-bold">
+              Role:
+              <br />
+            </p>
+            <div className="text-base">
+              <p>{project.role}</p>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p className="underline font-bold">
+              Responsibilities:
+              <br />
+            </p>
+            <div className="text-base over">
+              {/* <ul>
                                 {project.responsibilities.map((resp, index) => {
                                     <li key={index}>
                                         {resp}
                                     </li>
                                 })}
                             </ul> */}
-                            <PortableText value={project.responsibilities} />
-                        </div>
-                    </div>
-                </div>
-
-            )}
-
-            <div>
-                <Image src={project.image} alt={project.name}
-                    width={1920} height={1080}
-                    className="my-5 border-2 border-gray-700 object-cover rounded-xl" />
-
+              <PortableText value={project.responsibilities} />
             </div>
+          </div>
+        </div>
+      )}
 
-            <div className="pb-5">
-                <p className="underline font-bold">Lessons learned<br /></p>
+      <div>
+        {project.image ? (
+          <Image
+            src={project.image}
+            alt={project.name}
+            width={1920}
+            height={1080}
+            className="my-5 border-2  object-cover rounded-xl"
+          />
+        ) : (
+          <Image
+            src="/default.jpg"
+            alt={project.name}
+            width={200}
+            height={250}
+            className="my-5 border-2  object-cover rounded-xl"
+          />
+        )}
+      </div>
 
-                <PortableText value={project.lessonsLearned} />
+      <div className="pb-5">
+        <p className="underline font-bold">
+          Lessons learned
+          <br />
+        </p>
 
-            </div>
-
-
-        </div>)
+        <PortableText value={project.lessonsLearned} />
+      </div>
+    </div>
+  );
 }
--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -17,9 +17,9 @@ export const NavLink = ({ projects, blogs }: any) => {
 
   return (
     <div className="flex justify-between ">
-      <div id="projects" className="w-full text-center ">
+      <div id="projects" className="w-full text-center font-bold ">
         <p
-          className=" text-4xl font-bold sticky top-52 hover:text-[#f2cd87] transition duration-300 "
+          className=" sticky top-52 hover:text-[#f2cd87] transition duration-300 sm:text-md md:text-xl lg:text-4xl "
           onClick={() => scrollToSection(projectsRef)}
         >
           Projects
@@ -29,12 +29,12 @@ export const NavLink = ({ projects, blogs }: any) => {
       <div className="w-full">
         {/* Projects section */}
         <section ref={projectsRef} className="scroll-mt-10 text-black ">
-          <div className="flex flex-col items-center justify-between space-y-5 ">
+          <div className="grid grid-cols-1  gap-4">
             {projects.map((project: any) => (
               <Link
                 href={`/projects/${project.slug}`}
                 key={project._id}
-                className="group p-3 gap-3 flex justify-between transition rounded-md hover:bg-gray-300 "
+                className="group p-3 gap-3 flex flex-col justify-between items-center transition rounded-md hover:bg-gray-300 "
               >
                 <div className="flex items-center group-hover:scale-110 duration-100 ">
                   {project.image ? (
@@ -43,7 +43,7 @@ export const NavLink = ({ projects, blogs }: any) => {
                       alt={project.name}
                       width={200}
                       height={250}
-                      className="rounded-md max-w-md shadow-lg"
+                      className="rounded-md shadow-lg sm:max-w-sm md:max-w-md lg:max-w-lg"
                     />
                   ) : (
                     <Image
@@ -51,18 +51,23 @@ export const NavLink = ({ projects, blogs }: any) => {
                       alt={project.name}
                       width={200}
                       height={250}
-                      className="rounded-md max-w-md shadow-lg"
+                      className="rounded-md shadow-lg sm:max-w-sm md:max-w-md lg:max-w-lg"
                     />
                   )}
                 </div>
                 <div className="flex flex-col items-center w-[30vw]">
-                  <div className="font-extrabold ">{project.name}</div>
-
-                  <div className="font-semibold text-xs max-w-sm text-center">
+                  <div className="sm:text-sm sm:font-bold lg:text-xl lg:font-extrabold ">
+                    <p>{project.name}</p>
+                    {/* <span className="lg:inline-block sm:hidden">
+                      <br />
+                      <PortableText value={project.listOfTechnologies} />
+                    </span> */}
+                  </div>
+                  <div className="sm:text-xs sm:font-normal lg:font-semibold text-xs max-w-sm text-center">
                     <PortableText value={project.content} />
                   </div>
-                  <div className="font-base text-xs mt-3">
-                    <p className="underline font-medium">Technologies:</p>
+                  <div className="text-xs mt-3 text-center">
+                    <p className="underline ">Technologies</p>
                     <PortableText value={project.listOfTechnologies} />
                   </div>
                 </div>
